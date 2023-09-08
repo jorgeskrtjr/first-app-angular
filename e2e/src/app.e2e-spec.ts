7d4faceacd13e7bcfdb1f0bb486c030a84f6475f
Jorge Júnior <jorgesilvajunior10@gmail.com>
2023-09-07 23:58:57 -0300

    add: services

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,11 +1,32 @@
 import { browser, element, by, logging } from 'protractor';
 
-describe('first-app-lesson-01 app', () => {
+describe('first-app-lesson-09 app', () => {
 
   beforeEach(() => browser.get(''));
 
   it('should display correct title', async () => {
-    expect(await element.all(by.css('h1')).get(0).getText()).toEqual('Hello world!');
+    expect(await element.all(by.css('.brand-logo')).get(0).getAttribute('src')).toContain('/assets/logo.svg');
+  });
+
+  it('should have a filter string input', async () => {
+    expect(await element.all(by.css('input')).get(0).getAttribute('placeholder')).toEqual('Filter by city');
+    expect(await element.all(by.css('input')).get(0).getAttribute('type')).toEqual('text');
+  });
+
+  it('should have a search button', async () => {
+    expect(await element.all(by.css('button')).get(0).getText()).toEqual('Search');
+  });
+
+  it('should have a housing-location component', async () => {
+    expect(await element.all(by.css('.listing > .listing-location')).get(0).getText()).toEqual('Chicago, IL');
+  });
+
+  it('should have a title in the housing-location component', async () => {
+    expect(await element.all(by.css('.listing > h2')).get(0).getText()).toEqual('Acme Fresh Start Housing');
+  });
+
+  it('should have 10 housing-location components', async () => {
+    expect(await element.all(by.css('app-housing-location'))['length'] === 10);
   });
 
   afterEach(async () => {
